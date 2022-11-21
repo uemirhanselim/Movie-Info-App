dd596e7e15aee95c44df3fc8ebfc09600e70cbf8
uemirhanselim <uemirhanselim@gmail.com>
2022-11-21 11:41:59 +0300

    movie row completed and moved into seperate file

--- app/src/main/java/com/example/movieinfoapp/MainActivity.kt
+++ app/src/main/java/com/example/movieinfoapp/MainActivity.kt
@@ -3,34 +3,17 @@ package com.example.movieinfoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import coil.transform.CircleCropTransformation
 import com.example.movieinfoapp.model.Movie
 import com.example.movieinfoapp.model.getMovies
 import com.example.movieinfoapp.ui.theme.MovieInfoAppTheme
+import com.example.movieinfoapp.widgets.MovieRow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,52 +46,6 @@ fun HomeView(movieList: List<Movie> = getMovies()) {
             items(items = movieList) {
                 MovieRow(movie = it)
             }
-        }
-    }
-}
-
-@Composable
-fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
-
-    var isExpanded by remember {
-        mutableStateOf(false)
-    }
-
-    Card(modifier = Modifier
-        .padding(16.dp)
-        .fillMaxWidth()
-        .clickable { },
-        shape = RoundedCornerShape(corner = CornerSize(12.dp),),
-        elevation = 6.dp
-    ) {
-        Column() {
-            Row(modifier = Modifier.padding(8.dp)) {
-                Surface(
-                    modifier =Modifier.padding(12.dp)
-                        .size(100.dp), shape = RectangleShape,
-                    elevation =4.dp
-                ) {
-                    AsyncImage(model = ImageRequest.Builder(LocalContext.current).data(movie.poster)
-                        .transformations(CircleCropTransformation())
-                        .crossfade(true)
-                        .build(),
-
-                        contentDescription = "Poster")
-                }
-                Spacer(modifier = Modifier.width(5.dp))
-                Column(verticalArrangement = Arrangement.Center) {
-                    Text(text = movie.title)
-                    Text(text = movie.released)
-                    Text(text = movie.runtime)
-                }
-            }
-
-            Icon(imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
-                contentDescription = "Arrow Down",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .clickable { }
-                , tint = Color.DarkGray)
         }
     }
 }
---
+++ app/src/main/java/com/example/movieinfoapp/widgets/movie_row.kt
@@ -0,0 +1,101 @@
+package com.example.movieinfoapp.widgets
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.movieinfoapp.model.Movie
+import com.example.movieinfoapp.model.getMovies
+
+@Composable
+fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+
+    Card(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .clickable { },
+        shape = RoundedCornerShape(corner = CornerSize(12.dp),),
+        elevation = 6.dp,
+        backgroundColor = Color.LightGray
+    ) {
+        Column() {
+            Row(modifier = Modifier.padding(8.dp)) {
+                Surface(
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .size(100.dp), shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+                    elevation =4.dp
+                ) {
+                    AsyncImage(model = ImageRequest.Builder(LocalContext.current).data(movie.images[0])
+                        .crossfade(true)
+                        .build(),
+                        contentScale = ContentScale.FillBounds,
+                        contentDescription = "Poster")
+                }
+                Spacer(modifier = Modifier.width(5.dp))
+                Column(modifier = Modifier.align(Alignment.Top).padding(top = 12.dp)) {
+                    Text(text = movie.title, style = MaterialTheme.typography.h5, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.SemiBold)) {
+                            append("Released Year: ")
+                        }
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Light)) {
+                            append(movie.released)
+                        }
+                    })
+                    Text(text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.SemiBold)) {
+                            append("Genres: ")
+                        }
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Light)) {
+                            append(movie.genre)
+                        }
+                    })
+                }
+            }
+            AnimatedVisibility(visible = isExpanded,modifier = Modifier.padding(start = 16.dp, end = 8.dp)) {
+                Text(text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.SemiBold)) {
+                        append("Plot: ")
+                    }
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Light)) {
+                        append(movie.plot)
+                    }
+                })
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Icon(imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Arrow Down",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .clickable {
+                        isExpanded = !isExpanded
+                    }
+                , tint = Color.DarkGray)
+        }
+    }
+}
