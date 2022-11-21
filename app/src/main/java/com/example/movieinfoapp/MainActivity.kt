af36d0937c731a2e7ee16ffbbc82c9513e98c118
uemirhanselim <uemirhanselim@gmail.com>
2022-11-21 16:54:29 +0300

    mainActivity file has done

--- app/src/main/java/com/example/movieinfoapp/MainActivity.kt
+++ app/src/main/java/com/example/movieinfoapp/MainActivity.kt
@@ -3,24 +3,16 @@ package com.example.movieinfoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.movieinfoapp.model.Movie
-import com.example.movieinfoapp.model.getMovies
+import com.example.movieinfoapp.navigation.MovieNavigation
 import com.example.movieinfoapp.ui.theme.MovieInfoAppTheme
-import com.example.movieinfoapp.widgets.MovieRow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-
+                MovieNavigation()
             }
         }
     }
@@ -33,27 +25,10 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Composable
-fun HomeView(movieList: List<Movie> = getMovies()) {
-    Scaffold(
-        topBar = {
-            TopAppBar() {
-                Text(text = "Movies", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            }
-        }
-    ) {
-        LazyColumn() {
-            items(items = movieList) {
-                MovieRow(movie = it)
-            }
-        }
-    }
-}
 
-@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApp {
-        HomeView()
+
     }
 }
